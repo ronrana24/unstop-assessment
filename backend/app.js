35d36945139979939624dcd965e4f10657d182c6
rahulrana2000 <rahul.rana@haptik.ai>
2025-02-03 23:58:51 +0530

    URL updated

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173', 'https://unstop-assessment-backend.onrender.com/api/rooms/'] }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://unstop-assessment-frontend-eight.vercel.app/'] }));
 app.use('/api/rooms', roomRoutes);
 
 app.use((req, res) => {
